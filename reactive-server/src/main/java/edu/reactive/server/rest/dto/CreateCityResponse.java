3bcc927e844872cd76cd1559e7fb3be29ef88c9d
iushotin <iushotin@neoflex.ru>
2021-06-18 15:19:58 +0400

    improved logs

--- reactive-server/src/main/java/edu/reactive/server/rest/dto/CreateCityResponse.java
+++ reactive-server/src/main/java/edu/reactive/server/rest/dto/CreateCityResponse.java
@@ -1,13 +1,11 @@
 package edu.reactive.server.rest.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@NoArgsConstructor
+@AllArgsConstructor
 @Getter
-@Setter
 public class CreateCityResponse {
     @JsonProperty("status")
     private Boolean status;
--- reactive-server/src/test/java/edu/reactive/server/ReactiveServerApplicationTests.java
+++ reactive-server/src/test/java/edu/reactive/server/ReactiveServerApplicationTests.java
@@ -1,13 +1,41 @@
 package edu.reactive.server;
 
+import edu.reactive.server.repository.WeatherDataRepository;
+import edu.reactive.server.service.WeatherService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @SpringBootTest
 class ReactiveServerApplicationTests {
 
+	@MockBean
+	private WeatherDataRepository weatherDataRepository;
+
+	@Autowired
+	private WeatherService weatherService;
+
 	@Test
-	void contextLoads() {
+	void cities_2Cities() {
+
+		Mockito.when(weatherDataRepository
+				.findUniqueCities())
+				.thenReturn(Flux.just("City1", "City2"));
+
+		StepVerifier.create(weatherService.cities())
+				.assertNext(cityResponse -> {
+					Assertions.assertEquals("City1", cityResponse.getName());
+				})
+				.assertNext(cityResponse -> {
+					Assertions.assertEquals("City2", cityResponse.getName());
+				})
+				.expectComplete()
+				.verify();
 	}
 
 }
